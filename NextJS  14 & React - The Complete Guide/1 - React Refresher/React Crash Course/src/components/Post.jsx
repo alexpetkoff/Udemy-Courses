10dda4a51f02df596497daff48dc6d6980d7643b
Alexander Petkov <a.petkov@webrika.bg>
2024-04-02 16:43:30 +0300

    update Post.jsx to recieve props from App component

--- NextJS  14 & React - The Complete Guide/1 - React Refresher/React Crash Course/src/components/Post.jsx
+++ NextJS  14 & React - The Complete Guide/1 - React Refresher/React Crash Course/src/components/Post.jsx
@@ -1,3 +1,12 @@
-function Post() {}
+const names = ["Alexander", "Alexander The Great"];
+
+function Post({ name, text }) {
+    return (
+        <div>
+            <p>{name}</p>
+            <p>{text}</p>
+        </div>
+    );
+}
 
 export default Post;
