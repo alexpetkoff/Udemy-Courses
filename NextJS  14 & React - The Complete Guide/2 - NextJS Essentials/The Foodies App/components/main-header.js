bf329980e7f4ffec782789087239cab61ef2e12d
Alexander Petkov <a.petkov@webrika.bg>
2024-04-03 16:18:56 +0300

    finished main-header component

--- NextJS  14 & React - The Complete Guide/2 - NextJS Essentials/The Foodies App/components/main-header.js
+++ NextJS  14 & React - The Complete Guide/2 - NextJS Essentials/The Foodies App/components/main-header.js
@@ -2,13 +2,26 @@ import React from 'react'
 import Link from 'next/link'
 
 import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
 
 const MainHeader = () => {
     return (
-        <header>
-            <Link href='/'>
-                <img style={{ width: '100px', height: '100px' }} src={logoImg.src} alt="logo" />
+        <header className={classes.header}>
+            <Link className={classes.logo} href='/'>
+                <img style={{ width: '100px', height: '100px' }} src={logoImg.src} alt="A plate with food on it!" />
+                NextLevel Food
             </Link>
+
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href='/meals' >Browse Meals</Link>
+                    </li>
+                    <li>
+                        <Link href='/community' >Foodies Community</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
