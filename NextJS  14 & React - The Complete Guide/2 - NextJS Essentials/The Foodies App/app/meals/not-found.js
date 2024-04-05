354a877c507d6beeb8c85cfae3db65729f844d44
Alexander Petkov <iskander9207@icloud.com>
2024-04-05 13:52:06 +0300

    update

--- NextJS  14 & React - The Complete Guide/2 - NextJS Essentials/The Foodies App/app/meals/not-found.js
+++ NextJS  14 & React - The Complete Guide/2 - NextJS Essentials/The Foodies App/app/meals/not-found.js
@@ -1,6 +1,6 @@
 export default function NotFound() {
     return <main className="not-found">
         <h1>Not Found</h1>
-        <p>Unfortunately, we couldn't find the requested page or resource.</p>
+        <p>Unfortunately, we couldnt find the requested page or resource.</p>
     </main>
 }
