657f9189a3900c5730269f0335f05b3130dff36a
Alexander Petkov <iskander9207@icloud.com>
2024-06-02 16:54:29 +0300

    created news/slug page , not-found page for news and for main page

--- NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/news/page.jsx
+++ NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/news/page.jsx
@@ -1,3 +1,4 @@
+import { DUMMY_NEWS } from "@/dummy-news";
 import Link from "next/link";
 import React from "react";
 
@@ -6,15 +7,17 @@ function NewsPage() {
         <>
             <h1>News Page</h1>
             <ul className="news-list">
-                <li>
-                    <Link href="/news/link-1">Link 1</Link>
-                </li>
-                <li>
-                    <Link href="/news/link-2">Link 2</Link>
-                </li>
-                <li>
-                    <Link href="/news/link-3">Link 3</Link>
-                </li>
+                {DUMMY_NEWS.map((newsItem) => (
+                    <li key={newsItem.id}>
+                        <Link href={`/news/${newsItem.slug}`}>
+                            <img
+                                src={`/images/news/${newsItem.image}`}
+                                alt={newsItem.title}
+                            />
+                            <span>{newsItem.title}</span>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
     );
