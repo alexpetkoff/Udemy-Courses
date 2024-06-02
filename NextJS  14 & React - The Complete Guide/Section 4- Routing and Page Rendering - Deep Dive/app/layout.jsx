d584190a708faf2662e5661a92061aaeb4330d2e
Alexander Petkov <iskander9207@icloud.com>
2024-06-02 15:06:54 +0300

    created RootLayout and news, news/id pages

--- NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/layout.jsx
+++ NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/layout.jsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import MainHeader from "@/components/MainHeader";
+
 export const metadata = {
     title: "Next.js Page Routing & Rendering",
     description: "Learn how to route to different pages.",
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <MainHeader />
+                {children}
+            </body>
         </html>
     );
 }
--- NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/news/page.jsx
+++ NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/news/page.jsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 function NewsPage() {
     return (
-        <div id="home">
+        <>
             <h1>This is news page</h1>
-        </div>
+            <ul>
+                <li>
+                    <Link href="/news/link-1">Link 1</Link>
+                </li>
+                <li>
+                    <Link href="/news/link-2">Link 2</Link>
+                </li>
+                <li>
+                    <Link href="/news/link-3">Link 3</Link>
+                </li>
+            </ul>
+        </>
     );
 }
 
