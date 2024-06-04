ec7c29dca1082c19ff6e741b0bee04404618e09d
Alexander Petkov <iskander9207@icloud.com>
2024-06-04 11:04:12 +0300

    Update page.jsx
    fetching data from demo server

--- NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/(content)/news/page.jsx
+++ NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/(content)/news/page.jsx
@@ -1,12 +1,48 @@
-import React from "react";
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import NewsList from "@/components/news-list";
 
 function NewsPage() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [news, setNews] = useState();
+
+    useEffect(() => {
+        async function fetchNews() {
+            setIsLoading(true);
+            const res = await fetch("http://localhost:8080/news");
+            console.log(res);
+            if (!res.ok) {
+                setError("Failed to fetch data!");
+                setIsLoading(false);
+            }
+
+            const news = await res.json();
+            setIsLoading(false);
+            setNews(news);
+        }
+        fetchNews();
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    let newsContent;
+
+    if (news) {
+        newsContent = <NewsList news={news} />;
+    }
+
     return (
         <>
             <h1>News Page</h1>
-            <NewsList news={DUMMY_NEWS} />
+            {newsContent}
         </>
     );
 }
