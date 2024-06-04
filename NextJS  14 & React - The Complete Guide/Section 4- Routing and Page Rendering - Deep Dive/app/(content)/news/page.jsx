034a9e0578bffcf9cd1ebb04b4d6999b9a960725
Alexander Petkov <iskander9207@icloud.com>
2024-06-04 11:09:32 +0300

    Update page.jsx
    -fetching data from the server with server component

--- NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/(content)/news/page.jsx
+++ NextJS  14 & React - The Complete Guide/Section 4- Routing and Page Rendering - Deep Dive/app/(content)/news/page.jsx
@@ -1,48 +1,18 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
 import NewsList from "@/components/news-list";
 
-function NewsPage() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
-    const [news, setNews] = useState();
+async function NewsPage() {
+    const res = await fetch("http://localhost:8080/news");
 
-    useEffect(() => {
-        async function fetchNews() {
-            setIsLoading(true);
-            const res = await fetch("http://localhost:8080/news");
-            console.log(res);
-            if (!res.ok) {
-                setError("Failed to fetch data!");
-                setIsLoading(false);
-            }
-
-            const news = await res.json();
-            setIsLoading(false);
-            setNews(news);
-        }
-        fetchNews();
-    }, []);
-
-    if (isLoading) {
-        return <p>Loading...</p>;
+    if (!res.ok) {
+        throw new Error("Failed to fetch news!");
     }
 
-    if (error) {
-        return <p>{error}</p>;
-    }
-
-    let newsContent;
-
-    if (news) {
-        newsContent = <NewsList news={news} />;
-    }
+    const news = await res.json();
 
     return (
         <>
             <h1>News Page</h1>
-            {newsContent}
+            <NewsList news={news} />
         </>
     );
 }
