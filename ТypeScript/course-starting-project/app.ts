3c74981ceab0e6b50a8431b508578377e0dbc3a3
Alexander Petkov <iskander9207@icloud.com>
2024-06-11 17:55:18 +0300

    Typescript basics

--- ТypeScript/course-starting-project/app.ts
+++ ТypeScript/course-starting-project/app.ts
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+    name: "Alex",
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"],
+};
+
+console.log(person);
---
+++ ТypeScript/course-starting-project/basics.ts
@@ -0,0 +1,15 @@
+function add(n1: number, n2: number, show: boolean) {
+    if (show) {
+        return n1 + n2;
+    }
+
+    return null;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const showResult = true;
+
+const result = add(number1, number2, showResult);
+
+console.log(result);
