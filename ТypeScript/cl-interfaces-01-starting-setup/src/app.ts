a97926bade9deee426f37ac7ed54695fa74116f8
Alexander Petkov <iskander9207@icloud.com>
2024-06-19 15:18:14 +0300

    Update app.tsx
    class exercise!

--- ТypeScript/cl-interfaces-01-starting-setup/src/app.ts
+++ ТypeScript/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,40 @@
-class 
+class Department {
+    private id: string;
+    public name: string; // public is the default! not mandatory to initialize a prop with public infront!
+    private employees: string[] = []; //private means the method is not accessible outside the class Department.
+
+    constructor(name: string, id: string) {
+        this.name = name;
+        this.id = id;
+    }
+
+    describe() {
+        console.log("DEPARTMENT: " + this.name + ", " + this.id);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInfo() {
+        console.log("Number of employees: ", this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+let accounting = new Department("Accounting", " ID: 01");
+
+accounting.describe();
+
+// let accountingCopy = {
+//     name: "DUMMY",
+//     id: " ID: 02",
+//     describe: accounting.describe,
+// };
+
+// accountingCopy.describe();
+
+accounting.addEmployee("Aleks");
+accounting.addEmployee("Pesho");
+
+accounting.printEmployeeInfo();
