52018c77043e797108166c8af57ad246279d92ab
Alexander Petkov <iskander9207@icloud.com>
2024-07-04 11:19:49 +0300

    promise example

--- ТypeScript/generics/src/app.ts
+++ ТypeScript/generics/src/app.ts
@@ -1,1 +1,8 @@
 const names: Array<string> = ["Alexander", "Petkov"]; // string[]
+
+//promise example
+const promise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("This is done!");
+    }, 2000);
+});
