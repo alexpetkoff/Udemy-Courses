ec85b37206a0ac4df3431e98f05d77bef4c8c15b
Alexander Petkov <iskander9207@icloud.com>
2024-07-08 15:19:15 +0300

    added Done text

--- next-testing-intro-main/src/app/components/TodoItem/TodoItem.tsx
+++ next-testing-intro-main/src/app/components/TodoItem/TodoItem.tsx
@@ -1,34 +1,39 @@
-
-import { FaTrash } from "react-icons/fa"
-import { ChangeEvent, MouseEvent } from 'react'
+import { FaTrash } from "react-icons/fa";
+import { ChangeEvent, MouseEvent } from "react";
 // import updateTodo from "@/lib/updateTodo"
 // import deleteTodo from "@/lib/deleteTodo"
-import type { Todo } from "@/types/Todo"
-
+import type { Todo } from "@/types/Todo";
 
 type Props = {
-    todo: Todo,
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-}
+    todo: Todo;
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
 export default function TodoItem({ todo, setTodos }: Props) {
-
     const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
         //const updatedTodo = await updateTodo(todo)
-        setTodos(prevTodos => [...prevTodos.filter(prev => prev.id !== todo.id), { ...todo, completed: !todo.completed }])
-    }
+        setTodos((prevTodos) => [
+            ...prevTodos.filter((prev) => prev.id !== todo.id),
+            { ...todo, completed: !todo.completed },
+        ]);
+    };
 
     const handleDelete = async (e: MouseEvent<HTMLButtonElement>) => {
         //await deleteTodo(todo)
-        setTodos(prev => [...prev.filter(td => td.id !== todo.id)])
-    }
+        setTodos((prev) => [...prev.filter((td) => td.id !== todo.id)]);
+    };
 
     return (
         <article className="my-4 flex justify-between items-center">
-            <label className="text-2xl hover:underline" data-testid="todo-item" htmlFor={todo.id.toString()}>
+            <label
+                className="text-2xl hover:underline"
+                data-testid="todo-item"
+                htmlFor={todo.id.toString()}
+            >
                 {todo.title}
             </label>
             <div className="flex items-center gap-4">
+                {todo.completed && <p className="text-green-600">Done!</p>}
                 <input
                     type="checkbox"
                     checked={todo.completed}
@@ -41,10 +46,11 @@ export default function TodoItem({ todo, setTodos }: Props) {
                 <button
                     data-testid="delete-button"
                     onClick={handleDelete}
-                    className="p-3 text-xl rounded-2xl text-black border-solid border-black border-2 max-w-xs bg-red-400 hover:cursor-pointer hover:bg-red-300">
+                    className="p-3 text-xl rounded-2xl text-black border-solid border-black border-2 max-w-xs bg-red-400 hover:cursor-pointer hover:bg-red-300"
+                >
                     <FaTrash />
                 </button>
             </div>
         </article>
-    )
+    );
 }
