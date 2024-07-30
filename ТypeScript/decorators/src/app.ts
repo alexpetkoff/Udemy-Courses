8b2aef15a190d9982b3a75815c08e033faf2a08a
Alexander Petkov <iskander9207@icloud.com>
2024-07-30 16:27:00 +0300

    class decorators

--- ТypeScript/decorators/src/app.ts
+++ ТypeScript/decorators/src/app.ts
@@ -31,3 +31,47 @@ class Person {
 const person1 = new Person();
 
 console.log(person1);
+
+// --------------->
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log("property decorator");
+    console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+    console.log("Accessor decorator!");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        } else {
+            throw new Error("Invalid price - should be positive number!");
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
+
+const p1 = new Product("Book", 20);
+const p2 = new Product("Book - 2", 200);
+
+console.log(p1);
+console.log(p2);
