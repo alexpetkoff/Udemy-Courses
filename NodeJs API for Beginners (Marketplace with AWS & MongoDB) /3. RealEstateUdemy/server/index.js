974de083c1d833bf88ec93205c25e4f8a1ad3002
Alexander Petkov <iskander9207@icloud.com>
2024-11-19 14:35:21 +0200

    connect to mongoDB using mongoose

--- NodeJs API for Beginners (Marketplace with AWS & MongoDB) /3. RealEstateUdemy/server/index.js
+++ NodeJs API for Beginners (Marketplace with AWS & MongoDB) /3. RealEstateUdemy/server/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan"
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
+
+// connect to MongoDB
+mongoose.connect(process.env.DATABASE).then(
+    () => console.log('DB connected...')
+).catch(
+    (error) => console.log('DB connection error => ', error)
+)
 
 // middlewares
 app.use(cors());
