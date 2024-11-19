f2eacc255b31a82ee5f2ffbacb2c53f439442de3
Alexander Petkov <iskander9207@icloud.com>
2024-11-19 14:22:41 +0200

    create server with express

---
+++ NodeJs API for Beginners (Marketplace with AWS & MongoDB) /3. RealEstateUdemy/server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan"
+
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+    res.send(`The current time is: ${new Date().toLocaleTimeString()}`)
+});
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`)
+})
