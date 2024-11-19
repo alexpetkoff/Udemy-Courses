1022db6f8b780febfff7bebf54ed7e277063ef31
Alexander Petkov <iskander9207@icloud.com>
2024-11-19 15:51:50 +0200

    refactored the index.js code that starts the server

--- NodeJs API for Beginners (Marketplace with AWS & MongoDB) /3. RealEstateUdemy/server/index.js
+++ NodeJs API for Beginners (Marketplace with AWS & MongoDB) /3. RealEstateUdemy/server/index.js
@@ -1,29 +1,37 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import morgan from "morgan"
+import morgan from "morgan";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.js";
 
+const PORT = 8000;
 const app = express();
 
-// connect to MongoDB
-mongoose.connect(process.env.DATABASE).then(
-    () => console.log('DB connected...')
-).catch(
-    (error) => console.log('DB connection error => ', error)
-)
-
-// middlewares
+// Middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-//routes middlewares
-app.use("/api", authRoutes)
+// Routes middlewares
+app.use("/api", authRoutes);
 
-const PORT = 8000;
+// Function to start the server
+const startServer = async () => {
+    try {
+        // Connect to MongoDB
+        await mongoose.connect(process.env.DATABASE);
+        console.log("DB connected...");
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}!`)
-});
+        // Start the server
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}!`);
+        });
+    } catch (error) {
+        console.log("DB connection error =>", error);
+        process.exit(1); // Exit the process if the DB connection fails
+    }
+};
+
+// Start the server
+startServer();
--- NodeJs API for Beginners (Marketplace with AWS & MongoDB) /3. RealEstateUdemy/server/routes/auth.js
+++ NodeJs API for Beginners (Marketplace with AWS & MongoDB) /3. RealEstateUdemy/server/routes/auth.js
@@ -2,8 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+    res.send(`The current time is: ${new Date().toLocaleDateString()}`);
+})
+
 router.post("/login", (req, res) => {
-    res.json({ ...req.body, message: "Login successful!" })
+    res.json({ ...req.body, message: "Login successful!" });
 })
 
 export default router;
