085bd83c16f13aeec789eaace06c1adc814d8dbf
Alexander Petkov <iskander9207@icloud.com>
2024-12-12 10:41:23 +0200

    write / create new file with filesystem module

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
@@ -6,4 +6,7 @@ console.log(hello);
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); // takes two arguments - first the path to file, second the encoding type
 
 const textOut = `This is what we know about the avocado:\n${textIn}.`;
+
+fs.writeFileSync('./txt/output.txt', textOut);
+
 console.log(textOut);
