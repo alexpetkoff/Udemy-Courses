b216c1ce18184d01016413be77944b21105b44c0
Alexander Petkov <iskander9207@icloud.com>
2024-12-12 10:30:05 +0200

    update

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
@@ -2,3 +2,8 @@ const fs = require('fs');
 
 const hello = "Hello World!";
 console.log(hello);
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); // takes two arguments - first the path to file, second the encoding type
+
+const textOut = `This is what we know about the avocado:\n${textIn}.`;
+console.log(textOut);
