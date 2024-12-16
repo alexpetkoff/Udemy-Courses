119c390e1dbed8580a04276866c6b3d47f1f05fa
Alexander Petkov <iskander9207@icloud.com>
2024-12-16 15:26:52 +0200

    Update index.js
    add url module

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // FILES WITH NODEJS
 
@@ -33,6 +34,7 @@ const http = require("http");
 // ---------SERVER WITH NODEJS------------ //
 
 const server = http.createServer((req, res) => {
+  console.log(url);
   res.end("Hello from the server!");
 });
 
