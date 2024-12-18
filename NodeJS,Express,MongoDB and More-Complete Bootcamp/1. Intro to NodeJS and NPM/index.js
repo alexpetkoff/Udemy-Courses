22b26c64d405211bb406351b4e685f4323ac3b8b
Alexander Petkov <iskander9207@icloud.com>
2024-12-18 17:55:00 +0200

    templates replaced

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
@@ -33,21 +33,66 @@ const url = require("url");
 
 // ---------SERVER WITH NODEJS------------ //
 
+function replaceTemplate(template, product) {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%COUNTRY%}/g, product.country);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+
+  return output;
+}
+
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const data = fs.readFileSync("./dev-data/data.json", "utf-8");
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // OVERVIEW page
   if (pathName === "/overview" || pathName === "/") {
-    res.end("This is the overview page!");
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    const cardsHtmlArray = dataObject.map((product) => {
+      return replaceTemplate(templateCard, product);
+    });
+
+    res.end(templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtmlArray));
+
+    // PRODUCT page
   } else if (pathName === "/product") {
     res.end("This is product page!");
+
+    // API
   } else if (pathName === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
     res.end(data);
+
+    // NOT FOUND page
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",
