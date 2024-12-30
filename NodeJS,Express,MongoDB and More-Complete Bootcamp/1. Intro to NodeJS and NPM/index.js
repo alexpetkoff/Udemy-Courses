e6ba9e85dd010f436564f0f95fcebbfa12fffa96
alekspetkoff <iskander9207@icloud.com>
2024-12-30 16:17:53 +0200

    prettier formatter

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/1. Intro to NodeJS and NPM/index.js
@@ -40,75 +40,78 @@ const replaceTemplate = require("./modules/replaceTemplate");
 // ---------SERVER WITH NODEJS------------ //
 
 const templateOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+    `${__dirname}/templates/template-overview.html`,
+    "utf-8"
 );
 const templateCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  "utf-8"
+    `${__dirname}/templates/template-card.html`,
+    "utf-8"
 );
 const templateProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  "utf-8"
+    `${__dirname}/templates/template-product.html`,
+    "utf-8"
 );
 
 const data = fs.readFileSync("./dev-data/data.json", "utf-8");
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const { query, pathname } = url.parse(req.url, true);
+    const { query, pathname } = url.parse(req.url, true);
 
-  // OVERVIEW page
-  if (pathname === "/overview" || pathname === "/") {
-    res.writeHead(200, {
-      "content-type": "text/html",
-    });
+    // OVERVIEW page
+    if (pathname === "/overview" || pathname === "/") {
+        res.writeHead(200, {
+            "content-type": "text/html",
+        });
 
-    // console.log(
-    //   slugify("Fresh Avocados from our FARM!", {
-    //     lower: true,
-    //     replacement: "-",
-    //     strict: true,
-    //   })
-    // );
+        // console.log(
+        //   slugify("Fresh Avocados from our FARM!", {
+        //     lower: true,
+        //     replacement: "-",
+        //     strict: true,
+        //   })
+        // );
 
-    const cardsHtmlArray = dataObject.map((product) => {
-      return replaceTemplate(templateCard, product);
-    });
+        const cardsHtmlArray = dataObject.map((product) => {
+            return replaceTemplate(templateCard, product);
+        });
 
-    res.end(
-      templateOverview.replace("{%PRODUCT_CARDS%}", cardsHtmlArray.join(""))
-    );
+        res.end(
+            templateOverview.replace(
+                "{%PRODUCT_CARDS%}",
+                cardsHtmlArray.join("")
+            )
+        );
 
-    // PRODUCT page
-  } else if (pathname === "/product") {
-    const product = dataObject[query.id];
+        // PRODUCT page
+    } else if (pathname === "/product") {
+        const product = dataObject[query.id];
 
-    const output = replaceTemplate(templateProduct, product);
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.end(output);
+        const output = replaceTemplate(templateProduct, product);
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+        });
+        res.end(output);
 
-    // API
-  } else if (pathname === "/api") {
-    res.writeHead(200, {
-      "content-type": "application/json",
-    });
-    res.end(data);
+        // API
+    } else if (pathname === "/api") {
+        res.writeHead(200, {
+            "content-type": "application/json",
+        });
+        res.end(data);
 
-    // NOT FOUND page
-  } else {
-    res.writeHead(404, {
-      "Content-Type": "text/html",
-      "Test-Header": "Testing custom headers",
-    });
-    res.end("<h1>Page Not Found!</h1>");
-  }
+        // NOT FOUND page
+    } else {
+        res.writeHead(404, {
+            "Content-Type": "text/html",
+            "Test-Header": "Testing custom headers",
+        });
+        res.end("<h1>Page Not Found!</h1>");
+    }
 });
 
 const PORT = 8000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+    console.log(`Listening on port: ${PORT}`);
 });
