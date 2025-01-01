191e21ca78ab24c94dbab93e4b43b4399f45f337
alekspetkoff <iskander9207@icloud.com>
2025-01-01 18:59:44 +0200

    Streams example

---
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/2. How NodeJS works/streams.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const server = require("http").createServer();
+
+server.on("request", (req, res) => {
+    // Solution 1 without Streams
+    // fs.readFile("test-file.txt", (err, data) => {
+    //     if (err) console.log(err);
+
+    //     res.end(data);
+    // });
+
+    // Solution 2 with Streams
+    // const readable = fs.createReadStream("test-file.txt");
+
+    // readable.on("data", (chunk) => {
+    //     res.write(chunk);
+    // });
+
+    // readable.on("end", () => {
+    //     res.end("Reading finished!");
+    // });
+
+    // readable.on("error", (err) => {
+    //     console.log(err);
+    //     res.statusCode = 500;
+    //     res.end("File not found!");
+    // });
+
+    // Solution 3 - with backpressure fixing
+
+    const readable = fs.createReadStream("test-file.txt");
+    readable.pipe(res);
+});
+
+server.listen(8000, () => {
+    console.log("Server working on port: 8000");
+});
