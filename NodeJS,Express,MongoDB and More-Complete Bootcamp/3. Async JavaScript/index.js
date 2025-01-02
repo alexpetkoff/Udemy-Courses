88c5a0ce28d80f9e8dad23a53ca5198161f9eb92
Alexander Petkov <iskander9207@icloud.com>
2025-01-02 10:28:53 +0200

    example with new Promise()

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/3. Async JavaScript/index.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/3. Async JavaScript/index.js
@@ -1,13 +1,31 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('Could not find file!');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write a file!');
+      resolve('Success writing a file!');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`).then((data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .then((result) => {
-      console.log(result.body);
+      console.log(result.body.message);
 
       fs.writeFile('dog-img.txt', result.body.message, (err) =>
         console.log('Dog image saved!')
@@ -17,3 +35,20 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
       return console.log(error.message);
     });
 });
+
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((result) => {
+//       console.log(result.body);
+
+//       fs.writeFile('dog-img.txt', result.body.message, (err) =>
+//         console.log('Dog image saved!')
+//       );
+//     })
+//     .catch((error) => {
+//       return console.log(error.message);
+//     });
+// });
