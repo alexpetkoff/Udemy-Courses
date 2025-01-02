da47a5e5762a6c9023ed2d9d2f7040a650950917
Alexander Petkov <iskander9207@icloud.com>
2025-01-02 16:11:09 +0200

    delete Tour example

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
@@ -79,6 +79,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    res.status(404).json({
+      status: 'failed',
+      message: 'There is no tour with that id!',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port: ${port} 🚀`);
