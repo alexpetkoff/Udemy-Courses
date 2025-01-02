fa483f589308680639b38e95e547ae7cbea2ad46
Alexander Petkov <iskander9207@icloud.com>
2025-01-02 10:18:59 +0200

    example with then/catch

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/3. Async JavaScript/index.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/3. Async JavaScript/index.js
@@ -6,12 +6,14 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body);
+    .then((result) => {
+      console.log(result.body);
 
-      fs.writeFile('dog-img.txt', res.body.message, (err) =>
+      fs.writeFile('dog-img.txt', result.body.message, (err) =>
         console.log('Dog image saved!')
       );
+    })
+    .catch((error) => {
+      return console.log(error.message);
     });
 });
