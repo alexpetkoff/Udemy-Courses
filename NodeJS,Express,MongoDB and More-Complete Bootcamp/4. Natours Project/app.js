c3a67e6439af5969f5e8c7791a3846c5e1adf962
alekspetkoff <iskander9207@icloud.com>
2025-01-18 17:53:19 +0200

    created routes for users

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
@@ -112,21 +112,54 @@ const deleteTour = (req, res) => {
   });
 };
 
-// These are commented because in express and nodejs you can chain the request types if the route is the!
-// app.get('/api/v1/tours', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'Error',
+    message: 'This route is not yet defined',
+  });
+};
 
 // ROUTES
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
+
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 // SERVER STARTUP
 
