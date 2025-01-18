97bd4ed18aa150c832c425cb4968bdf9d3d205c3
alekspetkoff <iskander9207@icloud.com>
2025-01-18 17:55:48 +0200

    asd

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
@@ -118,24 +118,28 @@ const getAllUsers = (req, res) => {
     message: 'This route is not yet defined',
   });
 };
+
 const getUser = (req, res) => {
   res.status(500).json({
     status: 'Error',
     message: 'This route is not yet defined',
   });
 };
+
 const createUser = (req, res) => {
   res.status(500).json({
     status: 'Error',
     message: 'This route is not yet defined',
   });
 };
+
 const updateUser = (req, res) => {
   res.status(500).json({
     status: 'Error',
     message: 'This route is not yet defined',
   });
 };
+
 const deleteUser = (req, res) => {
   res.status(500).json({
     status: 'Error',
