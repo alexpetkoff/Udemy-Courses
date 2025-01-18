b7358164d37ed5ed8894ff8e9415a28ea90cffb2
alekspetkoff <iskander9207@icloud.com>
2025-01-18 18:38:56 +0200

    last

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
@@ -29,7 +29,4 @@ app.use('/api/v1/users', userRouter);
 
 // SERVER STARTUP
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port: ${port} 🚀`);
-});
+module.exports = app;
