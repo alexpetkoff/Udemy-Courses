c194a7f8b160fd8f01a63832e520394a3786b4e8
alekspetkoff <iskander9207@icloud.com>
2025-01-19 15:07:26 +0200

    added custom middleware for an example on how to chain middlewares for specific Request type

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/tourController.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/tourController.js
@@ -4,6 +4,30 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkId = (req, res, next, tourId) => {
+  if (tourId > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'There is no tour with that id!',
+    });
+  }
+
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  const { name, price } = req.body;
+
+  if (!name || !price) {
+    res.send(400, {
+      status: 'failed',
+      message: 'Name and price cannot be empty!',
+    });
+  }
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -18,13 +42,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = Number(req.params.id);
   const tour = tours.find((tour) => tour.id === id);
-
-  if (!tour) {
-    res.status(404).json({
-      status: 'failed',
-      message: 'There is no tour with that id!',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
