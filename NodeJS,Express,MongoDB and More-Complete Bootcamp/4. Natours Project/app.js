8e67a89b2c0ce8922b128e2c10ec4f00b815c522
alekspetkoff <iskander9207@icloud.com>
2025-01-19 15:13:38 +0200

    deleted test middleware

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/app.js
@@ -9,13 +9,7 @@ const app = express();
 // MIDDLEWARES
 
 app.use(morgan('dev'));
-
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware ;)');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
