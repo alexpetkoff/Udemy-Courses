63fb71ffc48403187d7f755b944d9b8f385c6689
alekspetkoff <iskander9207@icloud.com>
2025-04-13 14:45:31 +0300

    deleteTour Controller created

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/tourController.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/tourController.js
@@ -41,7 +41,7 @@ exports.createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
 
-    req.status(201).json({
+    res.status(200).json({
       status: 'success',
       data: {
         tour: newTour,
@@ -76,16 +76,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      message: 'Tour successfully deleted',
+    });
+  } catch (error) {
     res.status(404).json({
       status: 'failed',
       message: 'There is no tour with that id!',
     });
   }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
 };
