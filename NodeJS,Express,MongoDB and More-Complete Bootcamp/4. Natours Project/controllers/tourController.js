52a5db84d38bb6b346b2a298e95c8da1931326b3
alekspetkoff <iskander9207@icloud.com>
2025-04-18 18:12:37 +0300

    extended the logic in getAllTours controller to work with queries!

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/tourController.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/tourController.js
@@ -2,8 +2,18 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Build a query
+    const queryObject = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
 
+    excludedFields.forEach((el) => delete queryObject[el]);
+
+    const query = Tour.find(queryObject);
+
+    //Execute a query
+    const tours = await query;
+
+    //Send response
     res.status(200).json({
       status: 'success',
       results: tours.length,
