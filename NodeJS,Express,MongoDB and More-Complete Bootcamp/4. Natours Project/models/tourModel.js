c7f0921558706efe62dfbb0f315b8f4c86bee8ad
alekspetkoff <iskander9207@icloud.com>
2025-05-26 00:28:37 +0300

    cleaned out comments

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/models/tourModel.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/models/tourModel.js
@@ -101,12 +101,6 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-// tourSchema.post(/^find/, function (docs, next) {
-//   console.log(`Query took ${Date.now() - this.start} ms!`);
-
-//   next();
-// });
-
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
