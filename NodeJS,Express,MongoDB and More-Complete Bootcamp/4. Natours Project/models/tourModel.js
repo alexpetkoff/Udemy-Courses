bad06740bb2bcc9bafe086097bf14a1e988492e3
alekspetkoff <iskander9207@icloud.com>
2025-05-26 00:12:16 +0300

    added validation for tourSchema

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/models/tourModel.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/models/tourModel.js
@@ -7,6 +7,8 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A Tour must have a name!'],
       unique: true,
+      maxlength: [40, 'A name must be not more than 40 characters'],
+      minlength: [10, 'A name must be not less than 10 characters'],
     },
     slug: {
       type: String,
@@ -22,10 +24,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
