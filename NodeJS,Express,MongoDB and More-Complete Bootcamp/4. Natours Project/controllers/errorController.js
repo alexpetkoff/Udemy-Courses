3fef6a7daf50e3cc60209394c67d810c207101d7
alekspetkoff <iskander9207@icloud.com>
2025-06-01 17:30:25 +0300

    Refactoring: custom error messages for production and development mode

--- NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/errorController.js
+++ NodeJS,Express,MongoDB and More-Complete Bootcamp/4. Natours Project/controllers/errorController.js
@@ -2,10 +2,25 @@ module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.status = error.status || 'error';
 
-  res.status(error.statusCode).json({
-    status: error.status,
-    message: error.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+      stack: error.stack,
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    if (err.isOperational) {
+      res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+      });
+    } else {
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong!',
+      });
+    }
+  }
 
   next();
 };
